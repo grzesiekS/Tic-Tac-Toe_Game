3f0fef6162d527e4a1ea312b8af42f8757bfc890
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-06 18:31:43 +0200

    Add method addClassToElement in Player class

--- src/js/Player/Player.js
+++ src/js/Player/Player.js
@@ -1,10 +1,22 @@
 class Player {
-  constructor(playerName, gameOption){
+  constructor(playerName, gameOption, gameTurn=false){
     const thisPlayer = this;
     thisPlayer.playerName = playerName;
     thisPlayer.score = 0;
-    thisPlayer.gameTurn = false;
+    thisPlayer.gameTurn = gameTurn;
     thisPlayer.gameOption = gameOption;
+  }
+
+  addClassToElement(element) {
+    const thisPlayer = this;
+
+    /*START IF: If element classList value is not empty string */
+    if(element.classList.value == ''){
+      /*[DONE] Add class to element */
+      element.classList.add(...thisPlayer.gameOption);
+
+    /*END IF: If element classList value is not empty string */
+    }
   }
 }
 
