7a49d8f1993a9fd66f94aa57e6d5c0710640a428
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-06 17:18:48 +0200

    Add DataSource.js file

---
+++ src/js/DataSource.js
@@ -0,0 +1,16 @@
+export const wrappers = {
+  gameContent: '.game_content',
+};
+
+export const gameContent = {
+  gameContentField: '.game_content__field',
+  options: {
+    cross: 'fas fa-times',
+    circle: 'far fa-circle',
+  },
+  attributes: {
+    cross: 'cross',
+    circle: 'circle'
+  },
+};
+
