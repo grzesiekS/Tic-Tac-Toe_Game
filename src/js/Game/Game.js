0e83377a0815390d602113a1cbe12fcda51ebeb0
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-06 18:32:26 +0200

    Add js code for eventListener in Game class

--- src/js/Game/Game.js
+++ src/js/Game/Game.js
@@ -1,8 +1,14 @@
 import { gameContent } from '../DataSource.js';
+import Player from '../Player/Player.js';
 
 class Game {
   constructor(element) {
     const thisGame = this;
+
+    thisGame.players = {
+      player1: new Player('Player1',gameContent.options.cross, true),
+      player2: new Player('Player2',gameContent.options.circle),
+    };
 
     thisGame.getElement(element);
     thisGame.initActions();
@@ -14,6 +20,21 @@ class Game {
     thisGame.dom.gameContent.forEach(element => {
       element.addEventListener('click', function(){
         event.preventDefault();
+
+        /*START LOOP: For each player in players object */
+        for(let player in thisGame.players) {
+          /* Get player options from object players */
+          const playerOpt = thisGame.players[player];
+
+          /*START IF: If player has gameTurn true*/
+          if(playerOpt.gameTurn == true){
+            /*[DONE] Use Player method to add class to element i of game content */
+            playerOpt.addClassToElement(element.querySelector('i'));
+          /*END IF: If player has gameTurn true*/
+          }
+
+        /*END LOOP: For each player in players object */
+        }
       });
     });
   }
