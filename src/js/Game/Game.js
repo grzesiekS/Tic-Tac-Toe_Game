9729748080bc1300acdec9956052ad76c16de28a
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-06 17:55:06 +0200

    Create method initActions with EventListener in Game class

--- src/js/Game/Game.js
+++ src/js/Game/Game.js
@@ -5,6 +5,17 @@ class Game {
     const thisGame = this;
 
     thisGame.getElement(element);
+    thisGame.initActions();
+  }
+
+  initActions() {
+    const thisGame = this;
+
+    thisGame.dom.gameContent.forEach(element => {
+      element.addEventListener('click', function(){
+        event.preventDefault();
+      });
+    });
   }
 
   getElement(element) {
@@ -13,7 +24,7 @@ class Game {
     thisGame.dom = {};
     thisGame.dom.wrapper = element;
     thisGame.dom.gameContent = thisGame.dom.wrapper.querySelectorAll(gameContent.gameContentField);
-
+    console.log(thisGame.dom.gameContent);
   }
 }
 
