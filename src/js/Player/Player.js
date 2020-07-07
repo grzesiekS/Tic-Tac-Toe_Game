a91c94d762c3379bf2571249056ff6910a25ce4a
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-07 19:31:55 +0200

    Add addAtributeToDOMObject method in Player class

--- src/js/Player/Player.js
+++ src/js/Player/Player.js
@@ -13,6 +13,10 @@ class Player {
     if(element.innerHTML == '') {
       /*[DONE] Add gameOption to innerHTML */
       element.innerHTML = thisPlayer.gameOption;
+
+      /* Add atribute to DOM Object */
+      thisPlayer.addAtributeToDOMObject(element);
+
     /*END IF: If innerHTML of DOM Object is not equal '' */
     }
   }
@@ -29,6 +33,12 @@ class Player {
       thisPlayer.gameTurn = true;
     /*END IF: If Game Turn for the Player is true */
     }
+  }
+
+  addAtributeToDOMObject(element) {
+    const thisPlayer = this;
+
+    element.setAttribute('player-name', thisPlayer.playerName);
   }
 }
 
